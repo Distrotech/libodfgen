76a104fb9ae57f272fc2784bf84c9af245a132af
Fridrich Strba <fridrich.strba@bluewin.ch>
2006-11-28 17:38:20 +0000

    small fixes

--- wpd2sxw/wpd2sxw.cxx
+++ wpd2sxw/wpd2sxw.cxx
@@ -143,7 +143,7 @@ static bool writeContent(const char *pInFileName, GsfOutfile *pOutfile)
 	GSFInputStream input(pGsfInput);
 
 	WPDConfidence confidence = WPDocument::isFileFormatSupported(&input, false);
- 	if (confidence == WPD_CONFIDENCE_NONE)
+ 	if (confidence != WPD_CONFIDENCE_EXCELLENT)
  	{
  		fprintf(stderr, "ERROR: We have no confidence that you are giving us a valid WordPerfect document.\n");
  		return false;
