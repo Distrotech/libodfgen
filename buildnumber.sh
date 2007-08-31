c0df19f05db3e221c51e34eec37b9b4a9da55ca6
Fridrich Strba <fridrich.strba@bluewin.ch>
2007-08-31 15:46:51 +0000

    sometimes the script might be not executable, so remediate

--- buildnumber.sh
+++ buildnumber.sh
@@ -3,8 +3,8 @@
 # Script to create the BUILDNUMBER used by compile-resource. This script
 # needs the script createBuildNumber.pl to be in the same directory.
 
-{ ./createBuildNumber.pl \
-	wpd2odt/wpd2odt-build.stamp 
+{ perl ./createBuildNumber.pl \
+	wpd2sxw/wpd2sxw-build.stamp 
 #Success
 exit 0
 }
