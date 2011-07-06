853fdb131f44413eb375814339608f8b8275d2c4
Fridrich Štrba <fridrich.strba@bluewin.ch>
2011-07-06 18:25:32 +0200

    Some more ODG style mappings that LO understands and displays properly

--- filter/OdgGenerator.cxx
+++ filter/OdgGenerator.cxx
@@ -1052,7 +1052,9 @@ void OdgGeneratorPrivate::_writeGraphicsStyle()
 
 	TagOpenElement *pStyleGraphicsPropertiesElement = new TagOpenElement("style:graphic-properties");
 
-	if((mxStyle["draw:stroke"] && mxStyle["draw:stroke"]->getStr() == "none") || (mxStyle["svg:stroke-width"] && mxStyle["svg:stroke-width"]->getDouble() == 0.0))
+	if((mxStyle["draw:stroke"] && mxStyle["draw:stroke"]->getStr() == "none") ||
+	   (mxStyle["svg:stroke-width"] && mxStyle["svg:stroke-width"]->getDouble() == 0.0) ||
+	   (mxStyle["svg:stroke-color"] && mxStyle["svg:stroke-color"]->getStr() == "none"))
 		pStyleGraphicsPropertiesElement->addAttribute("draw:stroke", "none");
 	else
 	{
@@ -1061,6 +1063,7 @@ void OdgGeneratorPrivate::_writeGraphicsStyle()
 
 		if (mxStyle["svg:stroke-color"])
 			pStyleGraphicsPropertiesElement->addAttribute("svg:stroke-color", mxStyle["svg:stroke-color"]->getStr());
+
 		if (mxStyle["svg:stroke-opacity"] && mxStyle["svg:stroke-opacity"]->getDouble() != 1.0)
 			pStyleGraphicsPropertiesElement->addAttribute("svg:stroke-opacity", mxStyle["svg:stroke-opacity"]->getStr());
 
