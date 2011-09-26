ef6969464dfaf60c38dda41745b356d24da6474e
Fridrich Štrba <fridrich.strba@bluewin.ch>
2011-09-26 11:46:08 +0200

    If gradient type is not specified, assume linear

--- filter/OdgGenerator.cxx
+++ filter/OdgGenerator.cxx
@@ -1016,6 +1016,11 @@ void OdgGeneratorPrivate::_writeGraphicsStyle()
 			pDrawGradientElement->addAttribute("draw:style", mxStyle["draw:style"]->getStr());
 			pDrawOpacityElement->addAttribute("draw:style", mxStyle["draw:style"]->getStr());
 		}
+		else
+		{
+			pDrawGradientElement->addAttribute("draw:style", "linear");
+			pDrawOpacityElement->addAttribute("draw:style", "linear");
+		}
 		WPXString sValue;
 		sValue.sprintf("Gradient_%i", miGradientIndex);
 		pDrawGradientElement->addAttribute("draw:name", sValue);
